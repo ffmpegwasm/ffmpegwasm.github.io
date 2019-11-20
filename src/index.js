fce04182da5f06087f037d6928c04116a54333eb
jeromewu <jeromewus@gmail.com>
2019-11-20 13:10:53 +0000

    Add material-ui

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'material-design-icons/iconfont/material-icons.css';
+import 'typeface-roboto/index.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
