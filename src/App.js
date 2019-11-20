2ef43a883e358b742473b320ec152a4b75a9c133
jeromewu <jeromewus@gmail.com>
2019-11-20 12:04:31 +0000

    Add eslint

--- src/App.js
+++ src/App.js
@@ -7,9 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p />
         <a
           className="App-link"
           href="https://reactjs.org"
