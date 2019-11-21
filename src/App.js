2e246b3bf8373674324d73a2574f22bfb30ec35c
jeromewu <jeromewus@gmail.com>
2019-11-21 01:10:03 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          ffmpeg.js homepage
+          ffmpeg.js
         </p>
       </header>
     </div>
