1fe0a97aae292760481fb8cb35b8f13cf99a0b47
Jerome Wu <jeromewus@gmail.com>
2021-07-26 17:04:39 +0800

    Remove CDN part in installation

--- src/Installation.js
+++ src/Installation.js
@@ -4,13 +4,12 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Highlight, { defaultProps } from 'prism-react-renderer';
 import vsDark from 'prism-react-renderer/themes/vsDark';
-import { dependencies } from '../package.json';
 
 const NPM_INSTALL_SCRIPTS=`
 # Use npm
-$ npm install @ffmpeg/ffmpeg
+$ npm install @ffmpeg/ffmpeg @ffmpeg/core
 # Use yarn
-$ yarn add @ffmpeg/ffmpeg
+$ yarn add @ffmpeg/ffmpeg @ffmpeg/core
 `.trim('\n');
 
 const NPM_CORE_INSTALL_SCRIPTS=`
@@ -18,14 +17,6 @@ const NPM_CORE_INSTALL_SCRIPTS=`
 $ npm install @ffmpeg/core
 # Use yarn
 $ yarn add @ffmpeg/core
-`.trim('\n');
-
-const CDN_INSTALL_SCRIPTS=`
-<script src='https://unpkg.com/@ffmpeg/ffmpeg@${dependencies['@ffmpeg/ffmpeg'].slice(1)}/dist/ffmpeg.min.js'></script>
-<script>
-  const { createFFmpeg } = FFmpeg;
-  /* ... */
-</script>
 `.trim('\n');
 
 const useStyles = makeStyles({
@@ -68,13 +59,6 @@ function Installation() {
       <ReadOnlyEditor
         language="bash"
         code={NPM_INSTALL_SCRIPTS}
-      />
-      <Typography className={classes.para} variant="h6">
-        Or use a CDN
-      </Typography>
-      <ReadOnlyEditor
-        language="html"
-        code={CDN_INSTALL_SCRIPTS}
       />
       <Typography className={classes.para} variant="h6">
         If you want to use it in node.js, you need to install @ffmpeg/core as well.
